66cec10a38f1c0cdf0a0986e1873d4f8655007aa
Derek Desposito <derekdespo@icloud.com>
2020-06-16 20:14:52 -0400

    set up a second html for the quiz, with a second css page

---
+++ assets/scripts.js
@@ -0,0 +1,44 @@
+// These will serve as the questions being asked to the user who accepts taking the quiz
+var questions = [
+    {
+        title: "What is a good library for simplifying Javascript code?",
+        choices: ["jTheory", "jSonDerulo", "jQuery", "jQuantatamela"],
+        answer: "jQuery"
+    },
+    {
+        title: "How do you set a value for a variable? Do you use:",
+        choices: ["a semi-colon", "an equal sign", "two equal signs", "three equal signs"],
+        answer: "an equal sign"
+    },
+    {
+        title: "What kind of language is Javascript?",
+        choices: ["a styling language", "a formatting language", "an event language", "a foreign language"],
+        answer: "an event language"
+    },
+    {
+        title: "What data type includes the values true and false?",
+        choices: ["decimal", "string", "integer", "boolean"],
+        answer: "boolean"
+    },
+    {
+        title: "We can see the words 'if' and 'else' in what kind of statements?",
+        choices: ["Typical", "Conditional", "Irrational", "Loop"],
+        answer: "Conditional"
+    },
+    {
+        title: "Math.random generates a number between what?",
+        choices: ["1-10", "0-10", "0-1", "0-100"],
+        answer: "0-1"
+    },
+    {
+        title: "What does Math.floor do?",
+        choices: ["rounds up to the nearest integer", "rounds to the nearest integer", "rounds down to the nearest integer"],
+        answer: "rounds down to the nearest integer"
+    },
+    {
+        title: "In an HTML document, where does the Javascript code go?",
+        choices: ["on the bottom of the page", "in the header", "between the <script></script> tags", "in the css document"],
+        answer: "between the <script></script> tags"
+    },
+];
+
