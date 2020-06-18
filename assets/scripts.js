66ed91247adc7d37f56763664e0bd58326853494
Derek Desposito <derekdespo@icloud.com>
2020-06-17 23:45:49 -0400

    unfinished but got the questions up, wrong answer isnt working

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,5 @@
-// These will serve as the questions being asked to the user who accepts taking the quiz
+// This object will hold all the info for each question on the quiz
+// Will call on this information later in the code
 var questions = [
     {
         title: "What is a good library for simplifying Javascript code?",
@@ -42,3 +43,114 @@ var questions = [
     },
 ];
 
+// I set up a couple of elements on the quiz.html
+// We will need to tap into them, so that they can dynamically change as each question is displayed
+// Below is the tap in using querySelector
+// Connect to the container element
+var containerEl = document.querySelector(".container");
+// Connect to the timer element
+var timerEl = document.querySelector(".timer");
+// Connect to the section element with the class score
+var scoreEl = document.querySelector(".score");
+
+
+// Create dynamic elements
+var questBlock = document.createElement("p");
+
+// Global Elements
+var currentTime
+var index = 0
+var whatQ = -1
+var result = 0;
+
+
+// Set up a variable that can serve as the starting time
+var startTime = 100;
+// Create a function that allows the interval to decrease in decrements of one
+function setTime() {
+    // The variable timeInt will run a function that allows the startTime variable to decrease in intervals of one
+    var timeInt = setInterval(function () {
+        startTime--;
+        // This will allow the hooked timer element to have text printed into it 
+        timerEl.textContent = "Timer :  " + startTime + " " + "  seconds remaining";
+
+        if (startTime === 0 || startTime < 0) {
+            clearInterval(timeInt);
+            sendMessage();
+        }
+        else if (whatQ === questions.length) {
+            setTimeout();
+        }
+    }, 1000);
+}
+// This message should appear once the timer clock reaches zero
+function sendMessage() {
+    timerEl.textContent = "Loser!!";
+}
+setTime();
+
+// Have the questions display on the screen with buttons for the choices
+// Create a function for displaying the questions
+function nextQuest() {
+    // Declaring a variable that stores the current question from the questions object
+    var currentQuest = questions[index];
+    // Keep the container element empty
+    containerEl.textContent = "";
+    // Need to put the current question title in ther questBlock element
+    questBlock.textContent = currentQuest.title;
+    // Append questBlock to the Container Element so that it shows on the page
+    containerEl.appendChild(questBlock);
+    // Need to create an element that will hold the choices for each question
+    var answerDiv = document.createElement("div");
+    // A for loop can be used to make buttons for the choice options
+    for (let i = 0; i < currentQuest.choices.length; i++) {
+        var answerBtn = document.createElement("button");
+        answerBtn.classList.add("choiceBtn");
+        answerBtn.textContent = currentQuest.choices[i];
+        answerDiv.appendChild(answerBtn);
+    }
+
+    containerEl.appendChild(answerDiv)
+};
+
+nextQuest();
+
+
+function checkAnswer(event) {
+
+    if (event.target.matches(".choiceBtn")) {
+// Logic to compare if their answer is right
+var newDiv = document.createElement("div");
+newDiv.setAttribute("class", "ndiv");
+scoreEl.appendChild(newDiv);
+    };
+// if the answer is correct
+if (event.target.textContent === questions[index].answer) {
+    result++;
+    document.querySelector(".ndiv").innerHTML = "Nice Job!";
+    // wrong answer
+}
+else if (event.target.textContent !== questions[index].amswer) {
+    startTime = startTime - 15;
+    newDiv.querySelector(".ndiv").innerHTML = "Wrong Answer bud! Deduct 15 seconds from your score!";
+}
+// Next question
+
+index++
+nextQuest()
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Event to occur when a choice button is clicked
+document.addEventListener("click", checkAnswer);
